d4cb5a1cd1512cbb49cbc2cc84ee2491c609a778
akankshasawase <102587632+akankshasawase@users.noreply.github.com>
2022-05-11 22:33:04 +0530

    Add files via upload

---
+++ StudInfo.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class StudInfo {
+
+	public static void main(String[] args) {
+		
+	        Student1 obj = new Student1();
+	        obj.ss = "Akanksha";
+	        System.out.println(obj.ss);
+	        System.out.println(obj.name);
+
+	}
+
+}
